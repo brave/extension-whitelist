b2a908ee07ff4a21af2e72cd6eb5723fd83037ac
Mark Pilgrim <mpilgrim@brave.com>
2019-03-06 15:06:28 -0500

    more nits

--- scripts/gen_data_file.js
+++ scripts/gen_data_file.js
@@ -3,22 +3,22 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const CEWParser = require("../build/Release/ew_node_addon").CEWParser
+const CEWParser = require('../build/Release/ew_node_addon').CEWParser
 const addon = new CEWParser()
 
-const fs = require("fs")
-const data = fs.readFileSync("data/whitelist.json", "utf8")
-const blacklist = JSON.parse(String(data))["blacklist"]
+const fs = require('fs')
+const data = fs.readFileSync('data/whitelist.json', 'utf8')
+const blacklist = JSON.parse(String(data))['blacklist']
 for (var i in blacklist) {
-    addon.addToBlacklist(blacklist[i]["id"])
+    addon.addToBlacklist(blacklist[i]['id'])
 }
-const whitelist = JSON.parse(String(data))["whitelist"]
+const whitelist = JSON.parse(String(data))['whitelist']
 for (var i in whitelist) {
-    addon.addToWhitelist(whitelist[i]["id"])
+    addon.addToWhitelist(whitelist[i]['id'])
 }
 
 const serializedObject = addon.serialize()
-console.log("serializedObject == " + serializedObject)
-console.log("size == " + serializedObject.length)
+console.log('serializedObject == ' + serializedObject)
+console.log('size == ' + serializedObject.length)
 
-fs.writeFileSync("data/ExtensionWhitelist.dat", serializedObject)
+fs.writeFileSync('data/ExtensionWhitelist.dat', serializedObject)
